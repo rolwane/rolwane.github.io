7a68156f858718839ffe46dc36d7205c42460262
Rolwane Borges <rolwane@hotmail.com>
2021-11-05 18:47:37 -0300

    primeira versao atualizada da section home

--- js/script.js
+++ js/script.js
@@ -1,20 +1,57 @@
-function parallax() {
-    let scroll = document.documentElement.scrollTop;
-    document.querySelector('#bg-home').style.top = `${scroll * 0.5}px`;
-}
+const myName = 'Rolwane Borges';
+const occupation1 = 'Web Developer';
+const occupation2 = 'Designer';
+const span = document.querySelector('#write');
 
-function competenceAnimation() {
-    let scroll = document.documentElement.scrollTop;
-    let competence = document.querySelectorAll('.competence .fill');
-    if (scroll > (competence[0].offsetTop / 2)) {
-        competence[0].style.width = '90%';
-        competence[1].style.width = '75%';
-        competence[2].style.width = '50%';
-        competence[3].style.width = '20%';
+let count = 2;
+
+function typeWriter(phrase) {
+
+  setTimeout(() => {
+
+    for (let i = 0; i < phrase.length; i++) {
+      setTimeout(() => {
+        span.innerHTML += phrase[i];
+      }, 120 * i)
     }
+
+  }, 900)
+
+  setTimeout(() => {
+
+    clearSpan();
+    
+    if (count === 0) {
+      typeWriter(myName)
+      count = 1;
+      
+    } else if (count === 1) {
+      typeWriter(occupation1)
+      count = 2
+      
+    } else {
+      typeWriter(occupation2);
+      count = 0;
+    }
+    
+    
+
+  }, 5500)
 }
 
-window.addEventListener('scroll', () => {
-    parallax();
-    competenceAnimation();
-});
+function clearSpan() {
+  let letras = span.innerHTML.split('');
+  for (let i = 0; i < letras.length; i++) {
+    setTimeout(() => {
+      letras.pop();
+      span.innerHTML = letras.join('')
+    }, 40 * i);
+  }
+}
+
+let loop = setTimeout(() => {
+
+  clearSpan();
+  typeWriter(occupation1);
+
+}, 2500)
